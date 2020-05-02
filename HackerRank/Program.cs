4104ce94d1d78fc384c8e166bf6f0fd7ada34710
user <user@localhost>
2020-05-02 16:12:09 +0430

    Adding hourglass sum's solution

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -9,7 +9,17 @@ namespace HackerRank
             //var result = Warmup.SockMerchant.GetTotalMatchingPairs(9, new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
             //var result = Warmup.JumpingOnTheClouds.GetMinJumps(new[] { 0, 0, 1, 0, 0, 1, 0 });
             //var result = Warmup.CountingValleys.GetTotalValleys(8, "UDDDUDUU");
-            var result = Warmup.RepeatedString.GetNumberOfOccurrences("a", 1_000_000_000_000);
+            //var result = Warmup.RepeatedString.GetNumberOfOccurrences("a", 1_000_000_000_000);
+
+            var result = Arrays.HourglassSum.GetMaximumSum(new[] {
+                new[] { 1, 1, 1, 0, 0, 0 },
+                new[] { 0, 1, 0, 0, 0, 0 },
+                new[] { 1, 1, 1, 0, 0, 0 },
+                new[] { 0, 0, 2, 4, 4, 0 },
+                new[] { 0, 0, 0, 2, 0, 0 },
+                new[] { 0, 0, 1, 2, 4, 0 },
+            });
+
             Console.WriteLine(result);
             Console.ReadKey();
         }
