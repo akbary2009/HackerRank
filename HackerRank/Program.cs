9b9047538eabf56ffbcf112ad490d4299384a73c
user <user@localhost>
2020-05-02 10:27:21 +0430

    Adding jumping on the clouds' solution

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -6,7 +6,8 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            var result = Warmup.SockMerchant.GetTotalMatchingPairs(9, new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
+            //var result = Warmup.SockMerchant.GetTotalMatchingPairs(9, new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
+            var result = Warmup.JumpingOnTheClouds.GetMinJumps(new[] { 0, 0, 1, 0, 0, 1, 0 });
             Console.WriteLine(result);
             Console.ReadKey();
         }
---
+++ HackerRank/Warmup/JumpingOnTheClouds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Warmup
+{
+    public class JumpingOnTheClouds
+    {
+        public static int GetMinJumps(int[] c)
+        {
+            if (c?.Length < 2)
+                throw new ArgumentOutOfRangeException(nameof(c));
+            if (c.Any(cloud => !new[] { 0, 1 }.Contains(cloud)))
+                throw new ArgumentOutOfRangeException(nameof(c));
+            if (c.First() != 0 || c.Last() != 0)
+                throw new ArgumentOutOfRangeException(nameof(c));
+
+            var requiredSteps = 0;
+            var currentCloud = 0;
+            while (currentCloud < c.Length - 1)
+            {
+                var canTakeTwoJumps = (currentCloud + 2 < c.Length) && c.ElementAt(currentCloud + 2) == 0;
+                currentCloud += canTakeTwoJumps ? 2 : 1;
+                requiredSteps++;
+            }
+
+            return requiredSteps;
+        }
+    }
+}
