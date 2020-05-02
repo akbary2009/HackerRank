5f98dabcca187ab24877e4e8801d1c82e0f28b5d
user <user@localhost>
2020-05-02 11:00:29 +0430

    Adding counting valleys' solution

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -7,7 +7,8 @@ namespace HackerRank
         static void Main(string[] args)
         {
             //var result = Warmup.SockMerchant.GetTotalMatchingPairs(9, new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
-            var result = Warmup.JumpingOnTheClouds.GetMinJumps(new[] { 0, 0, 1, 0, 0, 1, 0 });
+            //var result = Warmup.JumpingOnTheClouds.GetMinJumps(new[] { 0, 0, 1, 0, 0, 1, 0 });
+            var result = Warmup.CountingValleys.GetTotalValleys(8, "UDDDUDUU");
             Console.WriteLine(result);
             Console.ReadKey();
         }
---
+++ HackerRank/Warmup/CountingValleys.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Warmup
+{
+    public class CountingValleys
+    {
+        private const char Uphill = 'u';
+        private const char Downhill = 'd';
+
+        public static int GetTotalValleys(int n, string s)
+        {
+            if (n < 2)
+                throw new ArgumentOutOfRangeException(nameof(n));
+            var steps = s?.Trim().ToLower().Select(step => step).ToArray();
+            if (steps.Any(step => !new[] { Uphill, Downhill }.Contains(step)))
+                throw new ArgumentOutOfRangeException(nameof(s));
+
+            var currentAltitude = 0;
+            var totalValleys = 0;
+            for (int i = 0; i < steps.Count(); i++)
+            {
+                var step = steps.ElementAt(i);
+                if (step == Uphill)
+                {
+                    currentAltitude++;
+                }
+                else
+                {
+                    if (currentAltitude == 0)
+                        totalValleys++;
+                    currentAltitude--;
+                }
+            }
+
+            return totalValleys;
+        }
+    }
+}
