9a301f5328d4deba251c50ae2921341b8a14ecf2
user <user@localhost>
2020-07-25 13:56:16 +0430

    Adding SuperReducedString

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -9,7 +9,9 @@ namespace HackerRank
             //var result = Warmup.SockMerchant.GetTotalMatchingPairs(9, new[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
             //var result = Warmup.JumpingOnTheClouds.GetMinJumps(new[] { 0, 0, 1, 0, 0, 1, 0 });
             //var result = Warmup.CountingValleys.GetTotalValleys(8, "UDDDUDUU");
-            var result = Warmup.RepeatedString.GetNumberOfOccurrences("a", 1_000_000_000_000);
+            //var result = Warmup.RepeatedString.GetNumberOfOccurrences("a", 1_000_000_000_000);
+
+            var result = Strings.SuperReducedString.Get("baab");
             Console.WriteLine(result);
             Console.ReadKey();
         }
---
+++ HackerRank/Strings/SuperReducedString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank.Strings
+{
+    public static class SuperReducedString
+    {
+        private const string Empty = "Empty String";
+
+        public static string Get(string s)
+        {
+            int numberOfReductions;
+            do
+            {
+                numberOfReductions = Reduce(ref s);
+            } while (numberOfReductions > 0);
+
+            return string.IsNullOrEmpty(s) ? Empty : s;
+
+
+            static int Reduce(ref string s)
+            {
+                var count = 0;
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    var set = s.Substring(i, 2);
+                    var areLowerCases = set.ToLower().Equals(set, StringComparison.Ordinal);
+                    var areMatches = set.Last().Equals(set.First());
+                    if (areLowerCases && areMatches)
+                    {
+                        count++;
+                        s = s.Remove(i, 2);
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
