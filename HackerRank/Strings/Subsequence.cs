ef758da90efb5b0a06c917b517c21b41be2ece95
user <user@localhost>
2020-07-27 15:31:16 +0430

    HackerRank in a String!

---
+++ HackerRank/Strings/Subsequence.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRank.Strings
+{
+    public static class Subsequence
+    {
+        public static string HasSubsequence(string s, string word = "hackerrank")
+        {
+            var found = true;
+            var stringIndex = 0;
+            foreach (var c in word)
+            {
+                var charIndex = s.Substring(stringIndex).IndexOf(c);
+                if (charIndex == -1)
+                {
+                    found = false;
+                    break;
+                }
+                stringIndex += charIndex;
+            }
+
+            return found ? "YES" : "NO";
+        }
+    }
+}
