ed769076d1143abe48f2c4c815aceeba8d64be79
user <user@localhost>
2020-07-28 20:49:05 +0430

    Fix: Highest Value Palindrome

--- HackerRank/Strings/Palindrome.cs
+++ HackerRank/Strings/Palindrome.cs
@@ -4,9 +4,6 @@ using System.Text;
 
 namespace HackerRank.Strings
 {
-    // TODO: It doesn't cover 5 test cases; complete the alogorithem to have creating a palindrome in priority.
-    // 2180212 2
-
     public static class Palindrome
     {
         private const string Impossible = "-1";
@@ -18,12 +15,9 @@ namespace HackerRank.Strings
             if (k <= 0)
                 return IsPalindrome(s) ? s : Impossible;
 
-            //var minNeededSwapsCount = (s.Substring(0, s.Length / 2))
-            //    .Zip(s.Substring((s.Length % 2 == 0 ? s.Length : s.Length + 1) / 2), (r, l) => r != l ? 1 : 0)
-            //    .Sum();
-
             var palindrome = new StringBuilder(s);
-            for (int rightIndex = 0; rightIndex <= s.Length / 2; rightIndex++)
+
+            for (int rightIndex = 0; rightIndex < s.Length / 2; rightIndex++)
             {
                 int leftIndex = s.Length - rightIndex - 1;
                 var rightChar = s[rightIndex];
@@ -31,29 +25,34 @@ namespace HackerRank.Strings
 
                 if (k <= 0)
                     break;
-                else if (k == 1)
+                else if (leftChar != rightChar)
                 {
-                    if (rightChar != leftChar)
-                    {
-                        leftChar = rightChar = new[] { leftChar, rightChar, }.Max();
-                        k--;
-                    }
-                    else if (rightIndex == leftIndex && rightChar < HighestNumber)
-                    {
-                        rightChar = leftChar = HighestNumber;
-                        k--;
-                    }
+                    leftChar = rightChar = new[] { leftChar, rightChar }.Max();
+                    k--;
                 }
-                else
-                {
-                    if (leftChar < HighestNumber) k--;
-                    if (rightChar < HighestNumber) k--;
-
-                    leftChar = rightChar = HighestNumber;
-                }
-
                 palindrome[rightIndex] = rightChar;
                 palindrome[leftIndex] = leftChar;
+            }
+
+            if (k > 0)
+            {
+                for (int rightIndex = 0; rightIndex <= s.Length / 2; rightIndex++)
+                {
+                    int leftIndex = s.Length - rightIndex - 1;
+                    var rightChar = palindrome[rightIndex];
+                    var leftChar = palindrome[leftIndex];
+
+                    if (k <= 0) break;
+                    var newK = k;
+                    if (leftChar < HighestNumber && leftChar == s[leftIndex]) newK--;
+                    if (rightChar < HighestNumber && rightChar == s[rightIndex] && rightIndex != leftIndex) newK--;
+
+                    if (newK >= 0)
+                    {
+                        k = newK;
+                        palindrome[rightIndex] = palindrome[leftIndex] = HighestNumber;
+                    }
+                }
             }
 
             return IsPalindrome(palindrome.ToString()) ? palindrome.ToString() : Impossible;
